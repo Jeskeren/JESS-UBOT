75e752041bef3b36fbf522d7c84344b11db0745d
Jeskeren <100347068+Jeskeren@users.noreply.github.com>
2022-02-26 18:48:02 +0800

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"😈JESS-UBOT😈 V7.0 [TELAH AKTIF BAJINGAN!]")
+    f"😈JESS-UBOT😈 V7.0 [TELAH AKTIF BAJINGAN🐱!]")
 
 
 if len(argv) not in (1, 3, 4):
